8496ca2337db8fe7a39e7044a90814eb3712da57
andrewbantly <andrewbantly@gmail.com>
2023-03-16 16:36:47 -0700

    game code along

---
+++ js/main.js
@@ -0,0 +1,128 @@
+// DOM SELECTORS
+const movementDisplay = document.querySelector("#movement");
+const statusDisplay = document.querySelector("#status");
+const canvas = document.querySelector("canvas");
+// console.log(movementDisplay, statusDisplay, canvas);
+
+// CANVAS SETUP
+const ctx = canvas.getContext("2d");
+// as the DOM what size the canvas actually is in pixels 
+// and set the canvas's resolution to be that size
+canvas.setAttribute('height', getComputedStyle(canvas).height);
+canvas.setAttribute('width', getComputedStyle(canvas).width);
+
+// console.log(ctx);
+
+// set renderer properties
+// ctx.fillStyle = "green";
+// // invoke renderer methods
+// // fillRect (x, y, width, height)
+// ctx.fillRect(10, 10, 100, 100)
+
+// set renderer properties
+// ctx.fillStyle = "purple";
+// // invoke renderer methods
+// ctx.fillRect(200, 100, 45, 200)
+
+// ctx.strokeStyle = "yellow";
+// ctx.lineWidth = 15;
+// ctx.strokeRect(25, 45, 50, 125);
+
+// // group context property setting and method calls together, as we see fit
+// function drawBox(x, y, w, h, color) {
+//     ctx.fillStyle = color;
+//     ctx.fillRect(x, y, w, h);
+// }
+// drawBox (50, 50, 35, 100, "orange");
+
+// canvas.addEventListener("click", e=> {
+//     console.log(`x: ${e.offsetX}, y: ${e.offsetY}`)
+//     drawBox(e.offsetX, e.offsetY, 30, 30, "blue");
+// })
+
+
+class Crawler {
+    constructor(x, y, width, height, color) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.alive = true;
+    }
+    render() {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+}
+
+
+// GAME OBJECTS
+const hero = new Crawler(0, 0, 45, 45, "hotpink");
+const ogre = new Crawler(150, 25, 100, 150, "#bada55");
+
+const gameLoopInterval = setInterval(gameLoop, 60)
+// const testCrawler = new Crawler(10, 45, 100, 50, "blue")
+// testCrawler.render()
+
+function handleKeyPressEvent(e) {
+    const speed = 10;
+    switch(e.key) {
+        case "w":
+        case "ArrowUp":
+            hero.y -= speed;
+            break
+        case "s":
+        case "ArrowDown":
+            hero.y += speed;
+            break
+        case "a":
+        case "ArrowLeft":
+            hero.x -= speed;
+            break
+        case "d":
+        case "ArrowRight":
+            hero.x += speed;
+            break
+    }
+    movementDisplay.innerText = `x: ${hero.x} y: ${hero.y}`
+}
+
+document.addEventListener("keydown", handleKeyPressEvent)
+
+function detectHit () {
+// axis aligned bounding box collision detection 
+// Google: AABB collision detection 
+    const left = ogre.x <= hero.width + hero.x;
+    const right = ogre.x + ogre.width >= hero.x;
+    const top = ogre.y <= hero.y + hero.height;
+    const bottom = ogre.y + ogre.height >= hero.y;
+    // console.log(left, right, top, bottom)
+    if (left && top && bottom && right) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+function gameLoop () {
+    // clear off the renderer 
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    // business logic of the game
+    // check for collision
+    if (detectHit()) {
+        // end the game here
+        console.log("end the game!")
+        ogre.alive = false
+        statusDisplay.innerText = "the hero has bravely slayed the ogre and survived!"
+        clearInterval(gameLoopInterval);
+        }
+    // check for end game conditions
+    // do all rendering 
+    if (ogre.alive) {
+        ogre.render();
+    }
+    hero.render();
+}
+
